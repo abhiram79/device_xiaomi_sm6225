47bdd71a41be02b4b7629fd9a6d9d48e1e1faa0b
rifsxd <rifat.44.azad.rifs@gmail.com>
2025-02-23 02:05:21 +0600

    [SQUASH] KernelSU: add manual hook support
    * Squash of these commits in this order:
    0c3c39a7663c76944e83b7f0f3043546fdcd489f KernelSU: update subproject reference and add KSU manual hook support CONFIG_KSU_MANUAL
    c4306be2093d4e9129976e4d1e490e467e624854 fs: remove CONFIG_KSU_MANUAL guards
    6a9b5a62a0727c2c0b9211d2d40aaac5f61f28fd kernelsu: refactor: scope-minimized manual hooks
    
    [Source] https://github.com/rifsxd/device_xiaomi_unified_kernel
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -601,6 +601,7 @@ extern bool ksu_devpts_hook;
 extern int ksu_handle_devpts(struct inode*);
 #endif
 
+
 /**
  * devpts_get_priv -- get private data for a slave
  * @pts_inode: inode of the slave
@@ -614,6 +615,7 @@ void *devpts_get_priv(struct dentry *dentry)
 		ksu_handle_devpts(dentry->d_inode);
 	}
 #endif
+
 
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
